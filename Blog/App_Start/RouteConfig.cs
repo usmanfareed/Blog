9e966b532ec4e142ec6af2fc262d534adf609b4b
DESKTOP-27VK2AK\Usman <usmanfareed92@gmail.com>
2016-11-02 01:14:38 +0500

    Revert "added theme of both blog and admin panel"
    This reverts commit a921643470d15cb15534d0e5ffd977d809697a81.

--- Blog/App_Start/RouteConfig.cs
+++ Blog/App_Start/RouteConfig.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using Blog.Controllers;
 
 namespace Blog
 {
@@ -14,9 +13,10 @@ namespace Blog
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            var namespaces = new[] { typeof(HomeController).Namespace };
-
-            routes.MapRoute("Default","{controller}/{action}/{id}",new { controller = "Home", action = "Index", id = UrlParameter.Optional  }, namespaces
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Blog/Areas/BlogSite/BlogSiteAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Blog.Areas.BlogSite
+{
+    public class BlogSiteAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "BlogSite";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "BlogSite_default",
+                "BlogSite/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
