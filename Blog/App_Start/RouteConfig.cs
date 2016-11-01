a921643470d15cb15534d0e5ffd977d809697a81
DESKTOP-27VK2AK\Usman <usmanfareed92@gmail.com>
2016-11-02 01:04:58 +0500

    added theme of both blog and admin panel

--- Blog/App_Start/RouteConfig.cs
+++ Blog/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Blog.Controllers;
 
 namespace Blog
 {
@@ -13,10 +14,9 @@ namespace Blog
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            var namespaces = new[] { typeof(HomeController).Namespace };
+
+            routes.MapRoute("Default","{controller}/{action}/{id}",new { controller = "Home", action = "Index", id = UrlParameter.Optional  }, namespaces
             );
         }
     }
